e3ac78f826027ddcb100bcdafb33405af11b6444
Alexey Burtsev <bur4ik85@mail.ru>
2023-03-28 00:32:13 +0300

    change bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import logging
 from aiogram import Bot, Dispatcher
 from config_data.config import Config, load_config
 from handlers import other_handlers, user_handlers
+from keyboards.set_menu import set_main_menu
 
 logger = logging.getLogger(__name__)
 
@@ -23,6 +24,8 @@ async def main():
 
     dp.include_router(user_handlers.router)
     dp.include_router(other_handlers.router)
+
+    await set_main_menu(bot)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
